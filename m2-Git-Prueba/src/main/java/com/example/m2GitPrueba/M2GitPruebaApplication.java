1ab5e4c047d21d6a2cc74d07c1cc538f87d4f56f
David Sans <dsans79@yahoo.es>
2021-10-05 09:16:33 +0200

    Initial commit

---
+++ m2-Git-Prueba/src/main/java/com/example/m2GitPrueba/M2GitPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.m2GitPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2GitPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2GitPruebaApplication.class, args);
+	}
+
+}
---
+++ m2-Git-Prueba/src/test/java/com/example/m2GitPrueba/M2GitPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m2GitPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2GitPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
